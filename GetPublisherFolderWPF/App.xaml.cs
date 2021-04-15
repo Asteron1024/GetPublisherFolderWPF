e80b9eae2f11027e1641ed68a989f5834339d2de
Alexander Martynov <alexander.martynov@dct.ua>
2021-04-15 16:16:00 +0300

    initial commit

---
+++ GetPublisherFolderWPF/App.xaml.cs
@@ -0,0 +1,8 @@
+﻿using System.Windows;
+
+namespace GetPublisherFolderWPF
+{
+    public partial class App : Application
+    {
+    }
+}
---
+++ GetPublisherFolderWPF/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Windows;
+using Windows.Storage;
+
+namespace GetPublisherFolderWPF
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += MainWindow_Loaded;
+        }
+
+        private async void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            var folder = Windows.Storage.ApplicationData.Current.GetPublisherCacheFolder("SomeFolder");
+            var file = await folder.CreateFileAsync("SomeFile", CreationCollisionOption.OpenIfExists);
+
+            using var fileStream = await file.OpenStreamForWriteAsync();
+            var buffer = Encoding.UTF8.GetBytes("SomeContent");
+            await fileStream.WriteAsync(buffer, 0, buffer.Length);
+        }
+    }
+}
